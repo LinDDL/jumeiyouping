21e44300b0f29c4fd7a05ee0ed07ba3e06669e8b
LinDDL <1744476938@qq.com>
2018-10-21 12:17:20 +0800

    第十八次更新聚美优品，登陆注册链接数据库

---
+++ php/res.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+    header("Content-Type:text/html;charset=utf-8;");
+    #1. 从前端拿来数据;
+    $username = @$_GET["username"];
+    $password = @$_GET["password"];
+    if($username == "" || $password == ""){
+        die("参数不全");
+    }
+    #2. 把数据放入到数据库之中;
+    // 1. 如何连接数据;
+    $con = mysql_connect("localhost","root","000000");
+    if(!$con){
+        // echo "数据库连接成功";
+        die("数据库连接失败");
+    }
+    mysql_select_db("userlist", $con);
+    // 判定是否存在相同用户名;
+    $result = mysql_query(
+                "SELECT username FROM 
+                detaillist WHERE username='$username'"
+    );
+    $count = 0;
+    while($row = mysql_fetch_array($result)){
+        $count ++;
+    }
+    // 用户名重名
+    if($count != 0){
+        die("用户名重名");
+    }
+    // 加密密码;
+    $password = md5($password);
+    mysql_query("INSERT INTO detaillist (password, username ) 
+    VALUES ('$password', '$username')");
+    if(mysql_error()){
+        die("数据库错误".mysql_error());
+    }
+    echo "注册成功";
+
+?>
